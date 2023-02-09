581c5807ca29e231fbfa52a4a23954262384397e
originiumLOL <101932525+originiumLOL@users.noreply.github.com>
2023-02-09 22:34:41 +0300

    Update main.js

--- menu/main.js
+++ menu/main.js
@@ -1,10 +1,10 @@
 // Get the modal
-var modal = document.getElementsByClassName("myModal");
+var modal = document.querySelectorAll("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementsByClassName("myImg");
-var modalImg = document.getElementsByClassName("img01");
-var captionText = document.getElementsByClassName("caption");
+var img = document.querySelectorAll("myImg");
+var modalImg = document.querySelectorAll("img01");
+var captionText = document.querySelectorAll("caption");
 img.onclick = function(){
   modal.style.display = "block";
   modalImg.src = this.src;
